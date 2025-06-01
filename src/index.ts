676a85972634de3381ba476c009973b018de53b5
Juan Diego <172089930+jdpadillavigo@users.noreply.github.com>
2025-06-01 13:23:36 -0500

    Trabajando con TODOs dinámicos

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express"
 import dotenv from "dotenv"
+import { listaTODOs, TODO } from "./data"
 
 dotenv.config()
 const app = express()
@@ -7,15 +8,33 @@ const app = express()
 const PORT = process.env.PORT
 
 app.get("/", (req : Request, resp : Response) => {
-    resp.send("Endpoint raiz")
+    resp.send("Endpoint raíz")
 })
 
 app.get("/todos", (req : Request, resp : Response) => {
-    const todos = [
-        { id : 1, descripcion : "Preparar la comida" },
-        { id : 2, descripcion : "Estudiar" },
-    ]
+    const todos = listaTODOs
     resp.json(todos)
+})
+
+app.get("/todos/:id", (req : Request, resp : Response) => {
+    const id = req.params.id
+
+    let todoEncontrado : TODO | null = null
+    for (let todo of listaTODOs) {
+        if (todo.id.toString() == id) {
+            todoEncontrado = todo
+            break
+        }
+    }
+
+    if (todoEncontrado == null) {
+        // Error: No se encontró
+        resp.status(400).json({
+            msg : "Código incorrecto"
+        })
+    }
+
+    resp.json(todoEncontrado)
 })
 
 app.listen(PORT, () => {
